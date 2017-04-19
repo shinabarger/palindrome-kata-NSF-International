154e9ae9dd29591914b5169a93845044749fc75a
shinabarger <davidshinabarger@gmail.com>
2017-04-19 11:32:27 -0400

    refactor, remove some fluff

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-
-    }
     public static Boolean palindrome(String exampleWord) {
 
-        char beginningOfWord;
-        char endOfWord;
         int lengthOfWord = exampleWord.length();
         exampleWord = exampleWord.toLowerCase();
 
-        for(int i = 0; i <= lengthOfWord; i++) {
+        for (int i = 0; i <= lengthOfWord; i++) {
 
             if (exampleWord.charAt(i) != exampleWord.charAt(lengthOfWord - 1)) {
                 return false;
@@ -19,7 +13,7 @@ public class Main {
             } else lengthOfWord -= 1;
 
         }
-        return  true;
+        return true;
     }
 
 }
